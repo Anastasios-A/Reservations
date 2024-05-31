e7800648f50170b1a4a89bc1920ad99478d3e254
Anastasios-A <anastasios_anastasopoulos@outlook.com>
2024-05-31 18:14:54 +0300

    Time Slots Css

--- src/components/TimeSlots/CalendarComponent/CalendarComponent.tsx
+++ src/components/TimeSlots/CalendarComponent/CalendarComponent.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from "./CalendarComponent.module.scss";
 
 interface CalendarComponentProps {
   onSelectDate: (date: Date) => void;
 }
 
-const CalendarComponent: React.FC<CalendarComponentProps> = ({ onSelectDate }) => {
+const CalendarComponent: React.FC<CalendarComponentProps> = ({
+  onSelectDate,
+}) => {
   return (
     <div>
-      <Calendar onClickDay={onSelectDate} />
+      <Calendar onClickDay={onSelectDate} className={styles.calentar} />
+      <footer className={styles.calendarFooter}>
+        <input id="day" type="checkbox" className={styles.checkbox} />
+        <label htmlFor="day" className={styles.label}>
+          {" "}
+          For every day
+        </label>
+      </footer>
     </div>
   );
 };
--- src/components/TimeSlots/TimeSlotsScreen/TimeSlotsScreen.tsx
+++ src/components/TimeSlots/TimeSlotsScreen/TimeSlotsScreen.tsx
@@ -1,33 +1,55 @@
 import { useState } from "react";
 import CalendarComponent from "../CalendarComponent/CalendarComponent";
 import TimeSlotsComponent from "../TimeSlotsComponent/TimeSlotsComponent";
+import styles from "./TimeSlotsScreen.module.scss";
 
-export default function TimeSlotsScreen(){
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-    const [isSaveButtonVisible, setIsSaveButtonVisible] = useState(false);
-  
-    const handleSelectDate = (date: Date) => {
-      setSelectedDate(date);
-      setIsSaveButtonVisible(false); // Hide save button when date changes
-    };
-  
-    const handleTimeSlotChange = (changed: boolean) => {
-      setIsSaveButtonVisible(changed);
-    };
+export default function TimeSlotsScreen() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [isSaveButtonVisible, setIsSaveButtonVisible] = useState(true);
 
-    return (
-        <div className="app-container">
-        <div className="calendar-container">
+  const handleSelectDate = (date: Date) => {
+    setSelectedDate(date);
+    setIsSaveButtonVisible(false); // Hide save button when date changes
+  };
+
+  const handleTimeSlotChange = (changed: boolean) => {
+    setIsSaveButtonVisible(changed);
+  };
+
+  return (
+    <div className={styles.availableSlots}>
+      <header>
+        <h1>Available Slots</h1>
+      </header>
+      <main className={styles.availableSlotsMain}>
+        <section className={styles.calendarContainer}>
           <CalendarComponent onSelectDate={handleSelectDate} />
-        </div>
-        <div className="timeslots-container">
-          <TimeSlotsComponent date={selectedDate} onTimeSlotChange={handleTimeSlotChange} />
-          {isSaveButtonVisible && (
-            <button className="save-button" onClick={() => alert('Save changes')}>Save</button>
-          )}
-        </div>
-      </div>
-      );
+        </section>
+        {selectedDate ? (
+          <section className={styles.timeslotsContainer}>
+            <TimeSlotsComponent
+              date={selectedDate}
+              onTimeSlotChange={handleTimeSlotChange}
+            />
+            <footer className={styles.buttons}>
+              <button className={styles.action + " " + styles.actionDecline}>
+                Cancel
+              </button>
 
-
+              {isSaveButtonVisible && (
+                <button
+                  className={styles.action + " " + styles.actionAccept}
+                  onClick={() => alert("Save changes")}
+                >
+                  Save
+                </button>
+              )}
+            </footer>
+          </section>
+        ) : (
+          <h4>Please select a date.</h4>
+        )}
+      </main>
+    </div>
+  );
 }
